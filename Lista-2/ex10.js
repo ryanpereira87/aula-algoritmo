7f2c6f4606d32b6a3ae04308ff67c8369229f01e
Ryan Pereira Alves <ryan.alves18@etec.sp.gov.br>
2025-09-04 08:22:16 -0300

    att

--- Lista-2/ex10.js
+++ Lista-2/ex10.js
@@ -1,24 +1,24 @@
 const input = require('readline-sync');
-let numero1= input.questionInt("Digite um número para gerar a tabuada: ");
-if(numero1<10)
-{let resultado1= numero1 * 1;
-let resultado2= numero1 * 2;
-let resultado3= numero1 * 3;
-let resultado4= numero1 * 4;
-let resultado5= numero1 * 5;
-let resultado6= numero1 * 6;
-let resultado7= numero1 * 7;
-let resultado8= numero1 * 8;
-let resultado9= numero1 * 9;
-let resultado10= numero1 * 10;
-console.log("O resultado é: ", resultado1)
-console.log("O resultado é: ", resultado2)
-console.log("O resultado é: ", resultado3)
-console.log("O resultado é: ", resultado4)
-console.log("O resultado é: ", resultado5)
-console.log("O resultado é: ", resultado6)
-console.log("O resultado é: ", resultado7)
-console.log("O resultado é: ", resultado8)
-console.log("O resultado é: ", resultado9)
-console.log("O resultado é: ", resultado10)
+let n1= input.questionInt("Digite um número para gerar a tabuada: ");
+if(n1<10)
+{let r1= n1 * 1;
+let r2= n1 * 2;
+let r3= n1 * 3;
+let r4= n1 * 4;
+let r5= n1 * 5;
+let r6= n1 * 6;
+let r7= n1 * 7;
+let r8= n1 * 8;
+let r9= n1 * 9;
+let r10= n1 * 10;
+console.log("O resultado é: ", r1)
+console.log("O resultado é: ", r2)
+console.log("O resultado é: ", r3)
+console.log("O resultado é: ", r4)
+console.log("O resultado é: ", r5)
+console.log("O resultado é: ", r6)
+console.log("O resultado é: ", r7)
+console.log("O resultado é: ", r8)
+console.log("O resultado é: ", r9)
+console.log("O resultado é: ", r10)
 }
